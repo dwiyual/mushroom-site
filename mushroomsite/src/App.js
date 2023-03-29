a7436aca012b16e11c144e0831c3a5cdb4846bde
dwiyual <dwiyual@gmail.com>
2023-03-29 04:27:40 -0500

    added components folder with some files and created a NavBar component

--- mushroomsite/src/App.js
+++ mushroomsite/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import backgroundImage from '../src/images/backgroundimg.png'
+import backgroundImage from '../src/images/backgroundimg.png';
+import NavBar from '../src/components/NavBarComponent';
 
 
 
@@ -8,13 +9,7 @@ import backgroundImage from '../src/images/backgroundimg.png'
 function App() {
   return (
     <div>
-      <nav>
-        <image>Logo Here</image>
-        <li><a href='#Home'>Home</a></li>
-        <li><a href='#GrowKit'>Grow Kit</a></li>
-        <li><a href='#Blog'>Blog</a></li>
-        <li><a href='#Contact'>Contact us</a></li>
-      </nav>
+     <NavBar></NavBar>
       <div className="App" style={{ backgroundImage: `url(${backgroundImage})` }}>
         <head>
           <div>
---
+++ mushroomsite/src/components/NavBarComponent.js
@@ -0,0 +1,15 @@
+function NavBar() {
+    return (
+        <nav id="NavBar">
+            <image>Logo Here</image>
+            <li><a href='#Home'>Home</a></li>
+            <li><a href='#GrowKit'>Grow Kit</a></li>
+            <li><a href='#Blog'>Blog</a></li>
+            <li><a href='#Contact'>Contact us</a></li>
+        </nav>
+    )
+
+}
+
+
+export default NavBar;
