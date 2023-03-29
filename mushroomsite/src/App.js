aae74a507fa6ac4b94e42023629d9a11bda2bffa
dwiyual <dwiyual@gmail.com>
2023-03-29 04:10:13 -0500

    adjusted css and changed size of background photo also rearranged the jsx to have the nav be its own section outside of main article

--- mushroomsite/src/App.js
+++ mushroomsite/src/App.js
@@ -7,19 +7,22 @@ import backgroundImage from '../src/images/backgroundimg.png'
 
 function App() {
   return (
-    <div className="App" style={{ backgroundImage: `url(${backgroundImage})` }}>
-      <head>
-        <nav>
-          <image>Logo Here</image>
-          <link>
-          <a href='#Home'>Home</a>
-          <a href='#GrowKit'>Grow Kit</a>
-          <a href='#Blog'>Blog</a>
-          <a href='#Contact'>Contact us</a>
-          </link>
-        </nav>
-      </head>
-      
+    <div>
+      <nav>
+        <image>Logo Here</image>
+        <li><a href='#Home'>Home</a></li>
+        <li><a href='#GrowKit'>Grow Kit</a></li>
+        <li><a href='#Blog'>Blog</a></li>
+        <li><a href='#Contact'>Contact us</a></li>
+      </nav>
+      <div className="App" style={{ backgroundImage: `url(${backgroundImage})` }}>
+        <head>
+          <div>
+            <p>Hello World</p>
+          </div>
+        </head>
+
+      </div>
     </div>
   );
 }
