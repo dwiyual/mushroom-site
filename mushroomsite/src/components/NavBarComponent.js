17f5954be088f439bbcc412e63d758aaa7f122dc
dwiyual <dwiyual@gmail.com>
2023-03-29 11:23:23 -0500

    added gradient to the background of nav and finished the nav component also got the size of the background to fit right

--- mushroomsite/src/components/NavBarComponent.js
+++ mushroomsite/src/components/NavBarComponent.js
@@ -1,11 +1,22 @@
 function NavBar() {
     return (
-        <nav id="NavBar">
+        <nav id="NavBar" style={{
+            backgroundImage:`gradient(to right, hex#60362a, hex#1c1718)`,
+        }}>
             <image>Logo Here</image>
-            <li><a href='#Home'>Home</a></li>
-            <li><a href='#GrowKit'>Grow Kit</a></li>
-            <li><a href='#Blog'>Blog</a></li>
-            <li><a href='#Contact'>Contact us</a></li>
+            <div id="NavLinks" style={{
+                display:`flex`,
+                justifyContent:`flex-end`,
+                marginLeft:`36rem`,
+                marginRight:`0`,
+                textDecoration:`none`,
+                
+            }}>
+                <ul><a href='#Home'>Home</a></ul>
+                <ul><a href='#GrowKit'>Grow Kit</a></ul>
+                <ul><a href='#Blog'>Blog</a></ul>
+                <ul><a href='#Contact'>Contact us</a></ul>
+            </div>
         </nav>
     )
 
