d78487e2cf16fc3982554bb46e945737d8b10a24
dwiyual <dwiyual@gmail.com>
2023-04-04 19:55:52 -0500

    wrote paragraph for home page describing the company, also need to correct the css to put a background on the paragraph

--- mushroomsite/src/components/TagLineComponent.js
+++ mushroomsite/src/components/TagLineComponent.js
@@ -1,14 +1,22 @@
 import '../index.css'
 
 function Tagline() {
-    return(
-        <p>
-            <h1 id="Tagline">Nature's pharmacy, delivered to your door!</h1>
-        </p>
+    return (
+        <div id='Company-Mission'>
+            <h1 className="Tagline">Nature's pharmacy, delivered to your door!</h1>
+            <p>Welcome to Nature's Pharmacy, your one-stop-shop for the best in medicinal and gourmet edible mushrooms. At Nature's Pharmacy, we're passionate about harnessing the power of mushrooms to promote optimal health and wellness. Our mission is simple: to provide our customers with the highest quality, sustainably-sourced mushroom products that help you feel your best, inside and out.
+
+                We believe that mushrooms are one of nature's most powerful superfoods, with a wealth of health benefits that have been known for centuries. From boosting your immune system to promoting healthy digestion and cognitive function, mushrooms have been used for medicinal purposes for thousands of years. But they're not just good for you - they're delicious, too! At Nature's Pharmacy, we offer a wide selection of gourmet edible mushrooms, perfect for cooking up a tasty and nutritious meal.
+
+                We take pride in sourcing our mushrooms from the finest growers and foragers around the world, using sustainable and ethical practices to ensure that our products are as good for the planet as they are for you. From our medicinal mushroom extracts to our gourmet truffle oils, each product is carefully crafted to deliver the highest quality and purity.
+
+                At Nature's Pharmacy, we're committed to providing exceptional customer service and support, and we're always happy to answer any questions you may have about our products or their benefits. We hope you'll join us in our mission to unlock the potential of mushrooms and live your healthiest, most vibrant life.
+            </p>
+        </div>
     )
-    
+
 }
-    
+
 
 
 export default Tagline;
